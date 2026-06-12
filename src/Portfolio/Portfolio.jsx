579c451e86bb23a27a22da2f03f53660d2234862
HoridasPro <horidassarker6@gmail.com>
2026-06-12 12:24:35 +0600

    updated to resume

--- src/Portfolio/Portfolio.jsx
+++ src/Portfolio/Portfolio.jsx
@@ -246,7 +246,7 @@ const Portfolio = () => {
     email: "horidassarker6@gmail.com",
     phone: "+880 1816553754",
     resumeLink:
-      "https://drive.google.com/file/d/1rX-UJ7pBbjJG9LZJjU1rAPyRFZMJi5L9/view?usp=sharing",
+      "https://drive.google.com/file/d/1aHrd65drxLZkkDayhyGtsQ1nT79n1VCe/view?usp=sharing",
     about:
       "Hi! I'm Haridas Sarker, a passionate Full Stack Developer with a love for building modern, scalable, and interactive web applications. Since 2021, I have been exploring MongoDB, Express.js, React, and Node.js to bring ideas to life with clean, performant code.",
     projects: [
